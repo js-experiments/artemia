bb3e00a8cf62d2bf80bcd3ac426c06c5b4742cab
k33g <ph.charriere@gmail.com>
2011-04-30 23:31:26 +0200

    jslint white: true, onevar: true, browser: true, undef: true, nomen: true, regexp: true, plusplus: true, bitwise: true, newcap: true, maxerr: 50, indent: 4

--- js/plugins/artemia.userdata.storage.js
+++ js/plugins/artemia.userdata.storage.js
@@ -1,11 +1,18 @@
 
+/* 04/30/11
+    jslint
+        white: true, onevar: true, browser: true, undef: true,
+        nomen: true, regexp: true, plusplus: true,
+        bitwise: true, newcap: true, maxerr: 50, indent: 4
+*/
+
 var artemia = (function (cyste) {
-    
+
     /*===========================================
         Navigator : IE
     ===========================================*/
 
-    function getUserDataStore(baseName,storeType){
+    function getUserDataStore(baseName, storeType) {
 
         /*
             from jsMag august 2010 article :
@@ -14,17 +21,16 @@ var artemia = (function (cyste) {
 
         */
         function getStorageEl() {
-            var elName = baseName;
+            var elName = baseName, el;
             /* We have to have a Dom element to operate on.
             If one already exists, we’ll use it. Otherwise,
             create it and attach the necessary behavior.*/
 
-            var el = document.getElementById(elName);
+            el = document.getElementById(elName);
             if (!el) {
                 el = document.createElement('div');
                 el.id = elName;
                 el.style.display = 'none';
-                //el.addBehavior('#default#userData');
                 el.style.behavior = 'url(\'#default#userData\')';
                 document.body.appendChild(el);
             }
@@ -32,114 +38,118 @@ var artemia = (function (cyste) {
         }
 
         return {
-            storeType:storeType,
-            baseName:baseName,
-            isAvailable:function(){
-                try{
+            storeType : storeType,
+            baseName : baseName,
+            isAvailable : function () {
+                try {
                     getStorageEl().load(this.baseName);
                     return true;
-                }catch(err){
-                    alert(err);
+                } catch (err) {
+                    //alert(err);
                     return false; /*not available*/
                 }
             },
 
-            get:function(key,callback){
+            get : function (key, callback) {
                 /* '|, :, ' are forbidden with IE i use '__' instead */
-                var obj , storageEl = getStorageEl();
+                var obj, storageEl = getStorageEl();
                 storageEl.load(this.baseName);
-                obj = JSON.parse(storageEl.getAttribute(this.baseName+'__'+key));
-                if(obj){obj.key=key;callback(obj);}else{callback(null);}
+                obj = JSON.parse(storageEl.getAttribute(this.baseName + '__' + key));
+                if (obj) {obj.key = key; callback(obj); } else { callback(null); }
             },
 
-            remove:function(keyOrObject,callback){
+            remove : function (keyOrObject, callback) {
                 /* '|, :, ' are forbidden with IE i use '__' instead */
                 var key = this.baseName + '__' + (typeof keyOrObject === 'string' ? keyOrObject : keyOrObject.key),
-                        storageEl = getStorageEl();
+                    storageEl = getStorageEl();
                 /*TODO: have to verify if exists before delete*/
                 storageEl.removeAttribute(key);
                 storageEl.save(this.baseName);
                 callback(key);
             },
 
-            save:function(obj,callback){
+            save : function (obj, callback) {
                 /* '|, :, ' are forbidden with IE i use '__' instead */
-                var id = this.baseName+'__'+(obj.key || cyste.guidGenerator()), storageEl;
+                var id = this.baseName + '__' + (obj.key || cyste.guidGenerator()), storageEl;
                 delete obj.key;
-                try{
+                try {
                     storageEl = getStorageEl();
-                    storageEl.setAttribute(id,JSON.stringify(obj));
+                    storageEl.setAttribute(id, JSON.stringify(obj));
                     storageEl.save(this.baseName);
 
                     obj.key = id.split('__')[1];
                     callback(obj);
-                }catch(err){throw(err);}
+                } catch (err) { throw (err); }
             },
 
-            all:function(callback){
-                var results = [],i,
-                        store=getStorageEl().XMLDocument.firstChild.attributes,
-                        l=store.length,id,key,baseName,obj;
+            all : function (callback) {
+                var results = [], i,
+                    store = getStorageEl().XMLDocument.firstChild.attributes,
+                    l = store.length, id, obj;
 
-                for (i = 0 ; i < l; ++i){
-                    id=store[i];
-
-                        obj = JSON.parse(id.nodeValue);
-
-                        obj.key = id.nodeName.split('__').slice(1).join("__");
-
-                        results.push(obj);
+                for (i = 0; i < l; i += 1) {
+                    id = store[i];
+                    obj = JSON.parse(id.nodeValue);
+                    obj.key = id.nodeName.split('__').slice(1).join("__");
+                    results.push(obj);
                 }
                 callback(results);
             },
 
-            drop:function(){
-                var m,that=this;
-                this.all(function(r){
-                    for(m in r){
-                        that.remove(r[m].key,function(){});
+            drop : function (callback) {
+                var that = this;
+                function something() {}
+                this.all(function (r) {
+                    var m;
+                    for (m in r) {
+                        if (r.hasOwnProperty(m)) {
+                            that.remove(r[m].key, something());
+                        }
                     }
                 });
                 callback();
             },
 
-            query:function(map,callback){
-                var results = [],res,m;
-                this.all(function(r){
-                    for(m in r){
-                        res = map(r[m]);
-                        if(res){results.push(res);}
+            query : function (map, callback) {
+                var results = [], res;
+                this.all(function (r) {
+                    var m;
+                    for (m in r) {
+                        if (r.hasOwnProperty(m)) {
+                            res = map(r[m]);
+                            if (res) { results.push(res); }
+                        }
                     }
                 });
-                if(callback){callback(results);}
+                if (callback) { callback(results); }
 
                 return {
-                    sum:function(callback){
-                        res=cyste.sum(results);
-                        if(callback){callback(res);}
+                    sum : function (callback) {
+                        res = cyste.sum(results);
+                        if (callback) { callback(res); }
                         return res;
                     },
-                    min:function(callback){
-                        res=cyste.min(results);
-                        if(callback){callback(res);}
+                    min : function (callback) {
+                        res = cyste.min(results);
+                        if (callback) { callback(res); }
                         return res;
                     },
-                    max:function(callback){
-                        res=cyste.max(results);
-                        if(callback){callback(res);}
+                    max : function (callback) {
+                        res = cyste.max(results);
+                        if (callback) { callback(res); }
                         return res;
                     }
-                }
+                };
 
             }
         };
-    };
+    }
 
     /*the _UPPER_ is a convention*/
-    cyste.get_USERDATA_store = function(baseName,storeType){
-        var store=getUserDataStore(baseName,storeType);
+    cyste.get_USERDATA_store = function (baseName, storeType) {
+        var store = getUserDataStore(baseName, storeType);
         cyste.interfaceIsImplemented(store);
-        if(!store.isAvailable()){store=null;}
+        if (!store.isAvailable()) { store = null; }
         return store;
     };
 
