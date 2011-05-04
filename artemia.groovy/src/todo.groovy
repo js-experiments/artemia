8af18a12b7087cba36ba69398852e1b67ca8d912
k33g <ph.charriere@gmail.com>
2011-05-04 08:38:20 +0200

    add future java and groovy version

---
+++ artemia.groovy/src/todo.groovy
@@ -0,0 +1,9 @@
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: k33g_org
+ * Date: 5/4/11
+ * Time: 8:10 AM
+ */
+
+println "To do ..."
---
+++ artemia.java/src/todo.java
@@ -0,0 +1,12 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: k33g_org
+ * Date: 5/4/11
+ * Time: 8:03 AM
+ */
+public class todo {
+
+    public static void main(String[] args) {
+        System.out.println("todo ...");
+    }
+}
